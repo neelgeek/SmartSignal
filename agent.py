f8622593774c828db25b9271674b74310048c97f
neelgeek <neelbhave91@gmail.com>
2018-10-28 00:58:58 +0530

    ?/

--- agent.py
+++ agent.py
@@ -1,8 +1,15 @@
 import numpy as np
 from envoirment import Env
 
+class agent:
 
-a = Env()
-a.print()
+    def __init__(self,eps=0.1, alpha=0.5):
+        self.eps = eps
+        self.alpha = 0.5
+        self.state_history =[]
+
+    def take_action(self):
+        
+        return 0
 
 
