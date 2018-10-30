297739229d80e3da1337f5cf709ca587e23a4912
neelgeek <neelbhave91@gmail.com>
2018-10-30 20:01:36 +0530

    First Draft of RL agent done

--- agent.py
+++ agent.py
@@ -7,9 +7,42 @@ class agent:
         self.eps = eps
         self.alpha = 0.5
         self.state_history =[]
+        self.state = np.zeros((100,10))
+        self.last_action = []
+        self.actions=list(range(10,110,10)) # generate a list from 10 to 100
 
-    def take_action(self):
+
+    def take_action(self,traf_den):
+          r = np.random.rand()
+        if r < self.eps :
+            #take random action i.e. explore
+            choice = (np.random.choice(10))  #take a random choice from the 6 possile actions
+            self.last_action = [traf_den,choice]
+            red_time = self.actions[choice]
+            
+        else :
+            #exploit (select the action which has max reward for the given state)
+            match_state = self.state[traf_den]
+            choice = np.argmax(match_state)
+            self.last_action = [traf_den,choice]
+            red_time = self.actions[choice] 
+            
         
-        return 0
+        return red_time
 
+           
+      
+
+
+    def on_red(self,traf_den):
+        #call take_action to take a action
+        return take_action(traf_den)
+
+    def on_reward(self,reward):
+        self.state[self.last_action[0],self.last_action[1]] += reward
+
+
+
+
+    
 
