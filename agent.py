667b4f5e21341ec2f2b14f1b4efb79fe93d6d8c7
neelgeek <neelbhave91@gmail.com>
2019-01-15 16:44:01 +0530

    Q-Learning Beta added

--- agent.py
+++ agent.py
@@ -4,62 +4,91 @@ import os,logging
 
 class agent:
 
-    def __init__(self,eps=0.7, alpha=0.5):
+    def __init__(self,eps=0.5, alpha=0.7):
         logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
         self.eps = eps
-        self.alpha = 0.3
-        if os.path.exists('./statespace.pickle'):
-            pickle_in = open('statespace.pickle','rb')
-            self.state=pickle.load(pickle_in)
-            logging.debug("Loaded Pickle")
-        else:
-            self.state = np.zeros((101,10),dtype=np.int64)
-            logging.debug("Made a new state")
+        self.alpha = alpha 
+        self.gamma = 1
+        self.state = self.load_model()
         self.last_action = []
         self.actions=list(range(10,110,10)) # generate a list from 10 to 100
+        self.Qmat = self.load_Q()
+        self.Prev_Q = []
+        self.CQ = []
+        self.QRew = 0
         
-
     def take_action(self,traf_den):
+        self.Prev_Q = self.CQ
         r = np.random.rand() # generating a random no. for eps greedy
         if r <= self.eps :
             print("Now Exploring")
             #take random action i.e. explore
             choice = (np.random.choice(10))  #take a random choice from the 6 possile actions
             self.last_action = [traf_den,choice]
+            self.CQ = [traf_den,choice]
             green_time = self.actions[choice]
             
         else :
             print("Now Exploiting")
             #exploit (select the action which has max reward for the given state)
-            match_state = self.state[traf_den]
+            # match_state = self.state[traf_den]
+            # choice = np.argmax(match_state)
+            match_state = self.Qmat[traf_den]
             choice = np.argmax(match_state)
             self.last_action = [traf_den,choice]
+            self.CQ = [traf_den,choice]
             green_time = self.actions[choice] 
             
         return green_time
 
-           
-      
-
-
     def on_red(self,traf_den):
         #call take_action to take a action
+        if self.Prev_Q:
+            self.update_Q(traf_den)
         return self.take_action(traf_den)
 
     def on_reward(self,reward):
         print("On Reward Called for ",self.last_action)
         if self.last_action:
             self.state[self.last_action[0],self.last_action[1]] += reward
+        self.QRew = reward
+    
+    def update_Q(self,new_state):
+        
+        Q_cur = self.Qmat[self.Prev_Q[0],self.Prev_Q[1]]
+        print(Q_cur)
+        Q_cur=Q_cur + self.alpha*(self.QRew+(self.gamma* np.max(self.Qmat[new_state]))-Q_cur)  
+        print(Q_cur)
+        self.Qmat[self.Prev_Q[0],self.Prev_Q[1]] = Q_cur
+        return 0
     
     def save_model(self):
         with open('statespace.pickle','wb') as f :
             np.savetxt("statespace.txt",self.state)
             pickle.dump(self.state,f)
+        
+        with open('Qmat.pickle','wb') as f:
+            np.savetxt("Qmat.txt",self.Qmat)
+            pickle.dump(self.Qmat,f)
             
-
-
-
-
-
+    def load_model(self): 
+        if os.path.exists('./statespace.pickle'):
+            pickle_in = open('statespace.pickle','rb')
+            state=pickle.load(pickle_in)
+            logging.debug("Loaded Pickle")
+        else:
+            state = np.zeros((101,10),dtype=np.int64)
+            logging.debug("Made a new state")
+        return state
     
+    def load_Q(self):
+        if os.path.exists('./Qmat.pickle'):
+            pickle_in = open('Qmat.pickle','rb')
+            state=pickle.load(pickle_in)
+            logging.debug("Loaded Pickle")
+        else:
+            state = np.zeros((101,10),dtype=np.int64)
+            logging.debug("Made a new state")
+        return state
+
 
